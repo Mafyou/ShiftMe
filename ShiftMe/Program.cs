a620e29daa56dff22a598e65845bc3c88015be60
Mafyou <mdupleix@hotmail.com>
2018-04-14 13:00:21 +0200

    Add project files.

---
+++ ShiftMe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
